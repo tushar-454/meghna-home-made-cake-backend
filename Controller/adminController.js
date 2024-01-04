74a2c98b0dc253f8576fdac987d0c08346e770ce
tushar-454 <imtushar454@gmail.com>
2024-01-04 23:44:19 +0600

    add all possible admin routes and controller, here controller is blank for internet issue

--- Controller/adminController.js
+++ Controller/adminController.js
@@ -1,7 +1,42 @@
+/**
+ * here getAll cakes get all cakes from database
+ */
 const getAllCakes = (req, res, next) => {
-  console.log('routes setup');
+  //
+};
+
+/**
+ * here addacake add a cake in database
+ */
+const addaCake = async (req, res, next) => {
+  //
+};
+
+/**
+ * here getAllOrders orders get all order from database
+ */
+const getAllOrders = async (req, res, next) => {
+  //
+};
+
+/**
+ * here updateOrderInfo update orders object infromation
+ */
+const updateOrderInfo = async (req, res, next) => {
+  //
+};
+
+/**
+ * here updateCakeInfo update cakes object infromation
+ */
+const updateCakeInfo = async (req, res, next) => {
+  //
 };
 
 module.exports = {
   getAllCakes,
+  addaCake,
+  getAllOrders,
+  updateOrderInfo,
+  updateCakeInfo,
 };
--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -2,5 +2,9 @@ const router = require('express').Router();
 const adminRoutes = require('../Controller/adminController');
 
 router.get('/getallcakes', adminRoutes.getAllCakes);
+router.post('/addacakes', adminRoutes.addaCake);
+router.get('/getallorders', adminRoutes.getAllOrders);
+router.patch('/updateorderinfo', adminRoutes.updateOrderInfo);
+router.patch('/updatecakeinfo', adminRoutes.updateCakeInfo);
 
 module.exports = router;
